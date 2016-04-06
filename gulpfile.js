0eca31f5d8ed4900c085ecc3cbd19a928445ed02
Penny Ren <emotionalecho@gmail.com>
2016-04-06 10:22:12 +0800

    fix gulp series excute task issue

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,14 @@ var webappDir = 'webapp';
 var cssDir = path.join(webappDir, '/css/');
 var commonCssDir = path.join(webappDir, '/_common/css/');
 
-gulp.task('default', ['clean', 'less-common', 'less', 'hbs-common', 'hbs', 'js-common', 'js', 'css-common', 'css']);
+gulp.task('default', ['clean', 'css-common', 'css', 'js-common', 'js']);
 
 gulp.task('watch', ['default'], function () {
-	gulp.watch(path.join(webappDir, '/tmpl/', '*.tmpl'), ['hbs', 'js']);
-	gulp.watch(path.join(webappDir, '/_common/tmpl/', '*.tmpl'), ['hbs-common', 'js-common']);
+	gulp.watch(path.join(webappDir, '/tmpl/', '*.tmpl'), ['js']);
+	gulp.watch(path.join(webappDir, '/_common/tmpl/', '*.tmpl'), ['js-common']);
 
-	gulp.watch(path.join(webappDir, '/less/', '*.less'), ['less', 'css']);
-	gulp.watch(path.join(webappDir, '/_common/less/', '*.less'), ['less-common', 'css-common']);
+	gulp.watch(path.join(webappDir, '/less/', '*.less'), ['css']);
+	gulp.watch(path.join(webappDir, '/_common/less/', '*.less'), ['css-common']);
 });
 
 gulp.task('clean', function () {
@@ -28,61 +28,65 @@ gulp.task('clean', function () {
 	var dir;
 	for (var i = 0; i < dirs.length ; i ++) {
 		dir = dirs[i];
-        // make sure the directories exists
-		if (!fs.existsSync(dir)) {
+       	if (!fs.existsSync(dir)) {
 			fs.mkdir(dir);
 		}
-        // delete the .css files
-		del.sync(dir + '*.css');
+        del.sync(dir + '*.css');
 	}
 });
 
 gulp.task("less", function () {
-	gulp.src(path.join(webappDir, '/less/', '*.less'))
+	var stream = gulp.src(path.join(webappDir, '/less/', '*.less'))
         .pipe(less())
         .pipe(gulp.dest(cssDir));
+    return stream;
 });
 
 gulp.task('less-common', function() {
-    gulp.src(path.join(webappDir,'/_common/less/', '*.less'))
+    var stream = gulp.src(path.join(webappDir,'/_common/less/', '*.less'))
         .pipe(less())
         .pipe(gulp.dest(commonCssDir));
+    return stream;
 });
 
 gulp.task('hbs', function () {
-	gulp.src(path.join(webappDir,'/tmpl/', '*.tmpl'))
+	var stream = gulp.src(path.join(webappDir,'/tmpl/', '*.tmpl'))
         .pipe(hbsPrecompile())
         .pipe(concat('templates.js'))
         .pipe(gulp.dest(path.join(webappDir, '/js/')));
+    return stream;
 });
 
 gulp.task('hbs-common', function() {
-    gulp.src(path.join(webappDir, '/_common/tmpl/', '*.tmpl'))
+    var stream = gulp.src(path.join(webappDir, '/_common/tmpl/', '*.tmpl'))
         .pipe(hbsPrecompile())
         .pipe(concat('templates.js'))
         .pipe(gulp.dest(path.join(webappDir, '/_common/js/')));
+    return stream;
 });
 
-gulp.task('js', function () {
-	gulp.src(path.join(webappDir, '/js/', '*.js'))
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest(publicDir));
-});
-
-gulp.task('js-common', function () {
-	gulp.src(path.join(webappDir, '/_common/js/', '*.js'))
-		.pipe(concat('build.js'))
-		.pipe(gulp.dest(path.join(publicDir, '/_common/')));
-});
-
-gulp.task('css', function () {
+gulp.task('css', ['less'], function () {
 	gulp.src(path.join(webappDir, '/css/', '*.css'))
 		.pipe(concat('app.css'))
 		.pipe(gulp.dest(publicDir));
 });
 
-gulp.task('css-common', function () {
+gulp.task('css-common', ['less-common'], function () {
 	gulp.src(path.join(webappDir, '/_common/css/', '*.css'))
 		.pipe(concat('build.css'))
 		.pipe(gulp.dest(path.join(publicDir, '/_common/')));
 });
+
+gulp.task('js', ['hbs'], function () {
+	gulp.src(path.join(webappDir, '/js/', '*.js'))
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest(publicDir));
+});
+
+gulp.task('js-common', ['hbs-common'], function () {
+	gulp.src(path.join(webappDir, '/_common/js/', '*.js'))
+		.pipe(concat('build.js'))
+		.pipe(gulp.dest(path.join(publicDir, '/_common/')));
+});
+
+
