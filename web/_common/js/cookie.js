7dc4ff1092384d8c026383f938444318b9747095
Penny Ren <emotionalecho@gmail.com>
2016-04-17 21:30:13 +0800

    add cookie support

--- web/_common/js/cookie.js
+++ web/_common/js/cookie.js
@@ -1,4 +1,56 @@
 var app = app || {};
+
 (function() {
 	app.cookie = {};
+	app.cookie.get = function (key) {
+		return cookie(key);
+	}
+
+	app.cookie.set = function (key, val, opts) {
+		cookie(key, val, opts);
+	}
+
+	app.cookie.remove = function (key, opts) {
+		if (!opts) opts = {};
+		cookie(key, '', extend(opts, {expires: -1}));
+	}
+
+	function cookie(key, val, opts) {
+		if (typeof val !== 'undefined') {
+			opts = extend({path: '/'}, opts);
+			if (typeof opts.expires == 'number') {
+				var expires =  new Date();
+				expires.setMilliseconds(expires.getMilliseconds() + opts.expires * 864e+5);
+				opts.expires = expires;
+			}
+			var domain = opts.domain ? '; domain=' + opts.domain : '';
+			var expires = opts.expires ? '; expires=' + opts.expires.toUTCString() : '';
+			var path = '; path=' + opts.path;
+			var secure = opts.secure ? '; secure' : '';
+			document.cookie = [key, '=', encodeURIComponent(val), expires, path, domain, secure].join('');
+
+		} else {
+			var cookies = document.cookie ? document.cookie.split('; ') : [];
+			var cookieVal;
+			var i = cookies.length;
+			if (!i) return null;
+			while (i--) {
+				var cookie = cookies[i];
+				if (cookie.indexOf(key) !== -1) {
+					cookieVal = decodeURIComponent(cookie.split('=')[1]);
+					break;
+				}
+			}
+			return cookieVal;
+		}
+	}
+
+	function extend(to, from) {
+		var keys = Object.keys(from);
+		var i = keys.length;
+		while (i--) {
+			to[keys[i]] = from[keys[i]];
+		}
+		return to;
+	}
 })();
